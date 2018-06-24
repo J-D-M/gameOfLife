e2e0a3038a88b71243e30fbed065b2339844bd8f
eyoson <eyoson@waifu.club>
2018-06-24 15:13:42 -0700

    refactored

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,20 @@
 
 const int width{720};
 const int height{720};
+const int cell_count{15}; // world will be cell_count x cell_count
 
 auto
 main(int argc, char **argv) -> int
 {
 	using namespace std;
 	using namespace sdl2;
+
+	int grid_len{static_cast<int>(height * 0.9)};
+	int gap{grid_len / cell_count}; // size of cell sides
+	grid_len -= grid_len % gap;     // clean up rounding errors
+
+	const tuple<int, int> corner{(width - grid_len) / 2,
+	                             (height - grid_len) / 2};
 
 	Window mainWin("Conway's Game of Life",
 	               Window::npos,
@@ -24,17 +32,9 @@ main(int argc, char **argv) -> int
 	Renderer mainRen(mainWin, -1, Renderer::accelerated);
 	Event    event;
 
-	int len{static_cast<int>(height * 0.9)};
+	World world(cell_count);
 
-	const int map_size{15};
-	const int gap{len / map_size};
-
-	len -= len % gap; // clean up rounding errors
-
-	const tuple<int, int> corner{(width - len) / 2, (height - len) / 2};
-
-	World world(map_size);
-
+	// main loop
 	for (bool quit{false}; !quit;) {
 		while (event.poll())
 			if (event.type() == SDL_QUIT)
@@ -44,8 +44,9 @@ main(int argc, char **argv) -> int
 		mainRen.clear();
 
 		mainRen.setDrawColor(color::white, 0xff);
-		mainRen.drawGrid(corner, len, gap);
-		mainRen.fillGrid(corner, len, gap, world.getMap());
+		mainRen.drawGrid(corner, grid_len, gap);
+		mainRen.fillGrid(corner, grid_len, gap, world.getMap());
+
 		mainRen.present();
 
 		world.update();
